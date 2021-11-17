edd73e2f28446f4dfff19daff8c1c7382fd12fd0
Dimaz55 <dmz55@ya.ru>
2021-11-17 12:53:37 +0600

    Задание 7.3

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def main():
-    info = []
-    file_list = ['1.txt', '2.txt', '3.txt']
+def sort_files_by_size(file_list):
+    content_list = []  # Список с содержимым файлов + заголовок
     for file in file_list:
         line_counter = 0
         content = ''
@@ -8,13 +7,24 @@ def main():
             for line in f:
                 line_counter += 1
                 content += line
-            info.append([f.name, line_counter, content])
-    info.sort(key=lambda x: x[1])
+            content_list.append([f.name, line_counter, content])
+    content_list.sort(key=lambda x: x[1])  # Сортировка по кол-ву строк
+    return content_list
 
-    with open('merged.txt', 'w', encoding='utf-8', ) as f:
-        for el in info:
-            for line in el:
+
+def merge_list_in_file(sorted_list, file):
+    with open(file, 'w', encoding='utf-8', ) as f:
+        for element in sorted_list:
+            for line in element:
                 f.write(str(line) + '\n')
+    return None
+
+
+def main():
+    file_list = ['1.txt', '2.txt', '3.txt']
+    sorted_list = sort_files_by_size(file_list)
+    merged_file = 'merged.txt'
+    merge_list_in_file(sorted_list, merged_file)
 
 
 if __name__ == '__main__':
